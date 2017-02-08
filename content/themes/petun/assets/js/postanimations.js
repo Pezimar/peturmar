1839d9e9073d230e3335de442a349095ba577e51
Pétur Már Sigurðsson <peturmarsig@gmail.com>
2017-02-08 19:10:14 +0000

    Version 1

--- content/themes/petun/assets/js/postanimations.js
+++ content/themes/petun/assets/js/postanimations.js
@@ -1,23 +1,47 @@
 $(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.postcontent_petun > p').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
-            }
-            
-        }); 
-    
-    });
-    
+	  $('.postcontent_petun > p').animate({'opacity':'0.1'}, 100);
+	  /* Every time the window is scrolled ... */
+	  $(window).scroll( function(){
+	  
+			/* Check the location of each desired element */
+			$('.postcontent_petun > p').each( function(i){
+				  
+				  var bottom_of_object = $(this).position().top;
+				  var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+				  /* If the object is completely visible in the window, fade it it */
+				  if( bottom_of_window - 80 > bottom_of_object ){
+						
+						$(this).animate({'opacity':'1'}, 300);
+
+				  }
+				  
+			});
+	  
+	  });
+	  var trigger = $('.hamburger'),
+		  overlay = $('.overlay'),
+	   isClosed = false;
+
+	  trigger.click(function () {
+		  hamburger_cross();		  
+	  });
+
+	  function hamburger_cross() {
+		  if (isClosed == true) {		 
+			overlay.hide();
+			trigger.removeClass('is-open');
+			trigger.addClass('is-closed');
+			isClosed = false;
+		  } else {   
+			overlay.show();
+			trigger.removeClass('is-closed');
+			trigger.addClass('is-open');
+			isClosed = true;
+		  }
+	}
+	
+	$('[data-toggle="offcanvas"]').click(function () {
+			$('#wrapper').toggleClass('toggled');
+	});   
 });
